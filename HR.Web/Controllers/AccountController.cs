a7046cdf7f6f11f2fd9b8e4dfc6ca4506a423ed2
Maruthi Burkani <maruthiburkani@gmail.com>
2017-11-14 16:32:39 +0530

    Add project files.

---
+++ HR.Web/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using HR.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace HR.Web.Controllers
+{
+    public class AccountController : BaseController
+    {
+        HrDataContext dbContext = new HrDataContext();
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            User userObj = dbContext.Users.Where(x => x.UserName == user.UserName && x.Password == user.Password).FirstOrDefault();
+
+            if (userObj != null) {
+                FormsAuthentication.SetAuthCookie(userObj.UserName,false);
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+            
+        }
+    }
+}

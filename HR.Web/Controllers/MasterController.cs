38480f21bd33d1a5267719f360c562324e7ee307
Maruthi Burkani <maruthiburkani@gmail.com>
2017-11-14 17:20:59 +0530

    Master Contoller : maruthi

---
+++ HR.Web/Controllers/MasterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HR.Web.Controllers
+{
+    public class MasterController : BaseController
+    {
+        // GET: Master
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}

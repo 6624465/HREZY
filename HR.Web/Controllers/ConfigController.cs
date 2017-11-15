22c574d3aaaf2fb1a86304df69935a1cd4710cde
Shruthi.T <tshruthi77@gmail.com>
2017-11-15 15:31:23 +0530

    designation changes

--- HR.Web/Controllers/ConfigController.cs
+++ HR.Web/Controllers/ConfigController.cs
@@ -11,12 +11,26 @@ namespace HR.Web.Controllers
     public class ConfigController : Controller
     {
         HrDataContext dbContext = new HrDataContext();
-        // GET: Config
-      
-        public  ActionResult Index()
-        {
 
-            return View();
+        public ActionResult GetDesignation()
+        {
+            var EmployeeData = dbContext.LookUps.Where(x => x.LookUpCategory == "EmployeeDesignation").AsQueryable();
+            return View(EmployeeData);
+        }
+        [HttpPost]
+        public ActionResult GetDesignationById(int lookupId)
+        {
+            var id = dbContext.LookUps.Where(x => x.LookUpID == lookupId).FirstOrDefault();
+            if (id == null)
+            {
+
+            }
+            else
+            {
+
+            }
+            dbContext.SaveChanges();
+            return RedirectToAction("GetDesignation");
         }
     }
 }

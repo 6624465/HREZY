73066f17e9f80bbdf21bed8345eb896838628318
vijay <vijay@sulabs.tech>
2017-11-15 13:24:25 +0530

    my changes

--- HR.Web/Helpers/SelectHelpers.cs
+++ HR.Web/Helpers/SelectHelpers.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using HR.Web.Models;
+
 namespace HR.Web.Helpers
 {
-    public static class SelectHelpers
+    public static class SelectListItemHelper
     {
+        public static IEnumerable<System.Web.Mvc.SelectListItem> EmployeeType()
+        {
+            using (var dbCntx = new HrDataContext())
+            {
+                return dbCntx.LookUps
+                                .Where(x => x.LookUpCategory == UTILITY.CONFIG_EMPLOYEETYPE)
+                                .Select(x => new System.Web.Mvc.SelectListItem {
+                                    Text = x.LookUpDescription,
+                                    Value = x.LookUpCode
+                                }).ToList<System.Web.Mvc.SelectListItem>().AsEnumerable();
+            }
+        }
     }
 }

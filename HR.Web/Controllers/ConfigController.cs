4c43debcec1b288f2e2000c17ae28f725ee06f01
Shruthi.T <tshruthi77@gmail.com>
2017-11-15 17:34:45 +0530

    desigfnation changes

--- HR.Web/Controllers/ConfigController.cs
+++ HR.Web/Controllers/ConfigController.cs
@@ -12,35 +12,94 @@ namespace HR.Web.Controllers
     [SessionFilter]
     public class ConfigController : BaseController
     {
-        HrDataContext dbContext = new HrDataContext();
 
-        public ActionResult GetDesignation()
+        #region Designation
+        public ActionResult EmployeeDesignationList()
         {
-            var EmployeeData = dbContext.LookUps.Where(x => x.LookUpCategory == "EmployeeDesignation").AsQueryable();
-            return View(EmployeeData);
-        }
-       
-        }
-        
-        public ActionResult EmpTypepaging(int page)
-        {
-            
-                int pageSize = 5;
-                int totalpages = 0;
-                int totalRecords = 0;
-            
-            using (var dbctx = new HrDataContext())
+            using (var dbContext = new HrDataContext())
             {
-
-                    totalRecords = dbctx.LookUps.ToList().Count();
-        
-                    totalpages = (totalRecords / pageSize) + ((totalRecords % pageSize) > 0 ? 1 : 0);
-   
-                   var allList = dbctx.LookUps.OrderBy(a => a.LookUpID).Skip((page - 1) * pageSize).Take(pageSize).ToList();
-                return View(allList);
+                var EmployeeData = dbContext.LookUps.Where(x => x.LookUpCategory == "EmployeeDesignation").ToList().AsEnumerable();
+                return View(EmployeeData);
             }
-           
         }
+
+        public PartialViewResult GetDesignationByID(int lookupID)
+        {
+            if (lookupID != -1)
+            {
+                using (var dbCntx = new HrDataContext())
+                {
+                    var employeeDesign = dbCntx.LookUps.Where(x => x.LookUpID == lookupID).FirstOrDefault();
+                    return PartialView(employeeDesign);
+                }
+
+            }
+            else
+                return PartialView(new LookUp { LookUpID = -1 });
+        }
+
+        [HttpPost]
+        public ActionResult SaveEmployeeDesignation(LookUp lookup)
+        {
+            if (lookup.LookUpID != -1)
+            {
+                using (var dbCntx = new HrDataContext())
+                {
+                    var _lookupObj = dbCntx.LookUps.Where(x => x.LookUpID == lookup.LookUpID).FirstOrDefault();
+
+                    _lookupObj.LookUpCode = lookup.LookUpCode;
+                    _lookupObj.LookUpDescription = lookup.LookUpDescription;
+                    _lookupObj.ModifiedBy = USERID;
+                    _lookupObj.ModifiedOn = DateTime.Now;
+
+                    dbCntx.SaveChanges();
+                }
+            }
+            else
+            {
+                using (var dbCntx = new HrDataContext())
+                {
+                    var lookupObj = new LookUp
+                    {
+                        LookUpCode = lookup.LookUpCode,
+                        LookUpDescription = lookup.LookUpDescription,
+                        LookUpCategory = UTILITY.CONFIG_EMPLOYEETYPE,
+                        IsActive = true,
+                        CreatedOn = DateTime.Now,
+                        CreatedBy = USERID,
+                        ModifiedOn = DateTime.Now,
+                        ModifiedBy = USERID
+                    };
+
+                    dbCntx.LookUps.Add(lookupObj);
+                    dbCntx.SaveChanges();
+                }
+
+            }
+            return RedirectToAction("GetDesignation");
+        }
+
+        #endregion
+
+        //public ActionResult EmpTypepaging(int page)
+        //{
+
+        //        int pageSize = 5;
+        //        int totalpages = 0;
+        //        int totalRecords = 0;
+
+        //    using (var dbctx = new HrDataContext())
+        //    {
+
+        //            totalRecords = dbctx.LookUps.ToList().Count();
+
+        //            totalpages = (totalRecords / pageSize) + ((totalRecords % pageSize) > 0 ? 1 : 0);
+
+        //           var allList = dbctx.LookUps.OrderBy(a => a.LookUpID).Skip((page - 1) * pageSize).Take(pageSize).ToList();
+        //        return View(allList);
+        //    }
+
+        //}
 
         #region EmployeeType
         [HttpGet]
@@ -64,6 +123,7 @@ namespace HR.Web.Controllers
                     var employeeType = dbCntx.LookUps.Where(x => x.LookUpID == lookupID).FirstOrDefault();
                     return PartialView(employeeType);
                 }
+             
             }
             else
                 return PartialView(new LookUp { LookUpID = -1 });

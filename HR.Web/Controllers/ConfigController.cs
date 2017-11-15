84a458d29ab272ea8dda132688f89961d3397f30
Kusumanjali <kusumanjalikorlpari@gmail.com>
2017-11-15 12:03:41 +0530

    EmployeeType

--- HR.Web/Controllers/ConfigController.cs
+++ HR.Web/Controllers/ConfigController.cs
@@ -11,21 +11,15 @@ namespace HR.Web.Controllers
     public class ConfigController : Controller
     {
         HrDataContext dbContext = new HrDataContext();
-        // GET: Config
-        public ActionResult EmployeeDesignation()
-        {
-         var  list = dbContext.LookUps.Where(x => x.LookUpCategory == "EmployeeDesignation").Select(m => new
-            {
-                LookUpCode = m.LookUpCode,
-                LookUpDescription = m.LookUpDescription,
-                LookUpID = m.LookUpID,
-                IsActive = m.IsActive
-            }).AsQueryable();
-            return Json(list,JsonRequestBehavior.AllowGet);
-        }
+       
         public  ActionResult Index()
         {
 
+            return View();
+        }
+        public ActionResult EmployeeType()
+        {
+           
             return View();
         }
     }

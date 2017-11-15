c4c3fa0a850d6ddf504b9234ebad5b691bf2c73d
vijay <vijay@sulabs.tech>
2017-11-15 11:34:25 +0530

    ACCOUNT CONTROLLER UPDATED

--- HR.Web/Controllers/AccountController.cs
+++ HR.Web/Controllers/AccountController.cs
@@ -24,6 +24,7 @@ namespace HR.Web.Controllers
 
             if (userObj != null) {
                 FormsAuthentication.SetAuthCookie(userObj.UserName,false);
+                USERID = user.Email;
                 return RedirectToAction("Index", "Home");
             }
             return View();

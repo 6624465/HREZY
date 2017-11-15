02e82675e43a326958a5b77cd8939eccf679f024
Shruthi.T <tshruthi77@gmail.com>
2017-11-15 12:01:09 +0530

    pull

--- HR.Web/Controllers/ConfigController.cs
+++ HR.Web/Controllers/ConfigController.cs
@@ -12,31 +12,11 @@ namespace HR.Web.Controllers
     {
         HrDataContext dbContext = new HrDataContext();
         // GET: Config
-        public ActionResult EmployeeDesignation()
-        {
-         var  list = dbContext.LookUps.Where(x => x.LookUpCategory == "EmployeeDesignation").Select(m => new
-            {
-                LookUpCode = m.LookUpCode,
-                LookUpDescription = m.LookUpDescription,
-                LookUpID = m.LookUpID,
-                IsActive = m.IsActive
-            }).AsQueryable();
-            return Json(list,JsonRequestBehavior.AllowGet);
-        }
+      
         public  ActionResult Index()
         {
-            //List<LookUp> l = new List<LookUp>();
-            LookUp l = new LookUp();
-            //var data = dbContext.LookUps.Where(x => x.LookUpCategory == "EmployeeDesignation").Select(m => new
-            //{
-            //    LookUpCode = m.LookUpCode,
-            //    LookUpDescription = m.LookUpDescription,
-            //    LookUpID = m.LookUpID,
-            //    IsActive = m.IsActive
-            //}).AsQueryable();
-
-             var data = dbContext.LookUps.Where(x => x.LookUpCategory == "EmployeeDesignation").AsQueryable();
-            return View(data); 
+           
+            return View(); 
         }
     }
 }

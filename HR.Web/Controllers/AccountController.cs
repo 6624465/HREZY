77f985a82594f21d0de3dfbbf7fc9bd5f5e2af4c
Maruthi Burkani <maruthiburkani@gmail.com>
2017-11-16 18:33:31 +0530

    Autherize filter added : maruthi

--- HR.Web/Controllers/AccountController.cs
+++ HR.Web/Controllers/AccountController.cs
@@ -22,13 +22,23 @@ namespace HR.Web.Controllers
         {
             User userObj = dbContext.Users.Where(x => x.UserName == user.UserName && x.Password == user.Password).FirstOrDefault();
 
-            if (userObj != null) {
-                FormsAuthentication.SetAuthCookie(userObj.UserName,false);
+            if (userObj != null)
+            {
+                FormsAuthentication.SetAuthCookie(userObj.UserName, false);
                 USERID = user.UserName;
                 return RedirectToAction("Index", "Home");
             }
             return View();
-            
+
+        }
+
+        [HttpGet]
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
         }
     }
 }
--- HR.Web/Controllers/AdministrationController.cs
+++ HR.Web/Controllers/AdministrationController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 
 namespace HR.Web.Controllers
 {
+    [Authorize]
     public class AdministrationController : Controller
     {
         // GET: Administration
@@ -25,61 +26,61 @@ namespace HR.Web.Controllers
             try
             {
 
-            using (HrDataContext dbContext = new HrDataContext())
-            {
-                Company company = new Company();
-
-                company.CompanyCode = companyVM.company.CompanyCode;
-                company.CompanyName = companyVM.company.CompanyName;
-                company.CreatedBy = "Admin";
-                company.CreatedOn = DateTime.Now;
-                company.InCorporationDate = companyVM.company.InCorporationDate;
-                company.IsActive = true;
-
-                if (companyVM.company.Logo.ContentLength > 0)
+                using (HrDataContext dbContext = new HrDataContext())
                 {
-                    company.CompanyLogo = companyVM.company.Logo.FileName;
-                    string path = Server.MapPath("~/Uploads/");
-                    if (!Directory.Exists(path))
+                    Company company = new Company();
+
+                    company.CompanyCode = companyVM.company.CompanyCode;
+                    company.CompanyName = companyVM.company.CompanyName;
+                    company.CreatedBy = "Admin";
+                    company.CreatedOn = DateTime.Now;
+                    company.InCorporationDate = companyVM.company.InCorporationDate;
+                    company.IsActive = true;
+
+                    if (companyVM.company.Logo.ContentLength > 0)
                     {
-                        Directory.CreateDirectory(path);
+                        company.CompanyLogo = companyVM.company.Logo.FileName;
+                        string path = Server.MapPath("~/Uploads/");
+                        if (!Directory.Exists(path))
+                        {
+                            Directory.CreateDirectory(path);
+                        }
+                        companyVM.company.Logo.SaveAs(path + company.CompanyLogo);
                     }
-                    companyVM.company.Logo.SaveAs(path + company.CompanyLogo);
-                }
 
-                company.RegNo = companyVM.company.RegNo;
-                company.ModifiedBy = "Admin";
-                company.ModifiedOn = DateTime.Now;
+                    company.RegNo = companyVM.company.RegNo;
+                    company.ModifiedBy = "Admin";
+                    company.ModifiedOn = DateTime.Now;
 
 
-                Address companyAddress = new Address()
-                {
-                    Address1 = addressVm.Address1,
-                    Address2 = addressVm.Address2,
+                    Address companyAddress = new Address()
+                    {
+                        Address1 = addressVm.Address1,
+                        Address2 = addressVm.Address2,
 
-                    AddressType = "Company",
-                    CityName = addressVm.CityName,
-                    Contact = addressVm.Contact,
-                    CountryCode = addressVm.CountryCode,
-                    LinkID = company.CompanyId,
-                    CreatedBy = "Admin",
-                    CreatedOn = DateTime.Now,
-                    Email = addressVm.Email,
-                    FaxNo = addressVm.FaxNo,
-                    MobileNo = addressVm.MobileNo,
-                    IsActive = true,
-                    ModifiedBy = "Admin",
-                    ModifiedOn = DateTime.Now,
-                    SeqNo = addressVm.SeqNo,
-                    StateName = addressVm.StateName,
-                    TelNo = addressVm.TelNo,
-                    WebSite = addressVm.WebSite,
-                    ZipCode = addressVm.WebSite,
-                };
+                        AddressType = "Company",
+                        CityName = addressVm.CityName,
+                        Contact = addressVm.Contact,
+                        CountryCode = addressVm.CountryCode,
+                        LinkID = company.CompanyId,
+                        CreatedBy = "Admin",
+                        CreatedOn = DateTime.Now,
+                        Email = addressVm.Email,
+                        FaxNo = addressVm.FaxNo,
+                        MobileNo = addressVm.MobileNo,
+                        IsActive = true,
+                        ModifiedBy = "Admin",
+                        ModifiedOn = DateTime.Now,
+                        SeqNo = addressVm.SeqNo,
+                        StateName = addressVm.StateName,
+                        TelNo = addressVm.TelNo,
+                        WebSite = addressVm.WebSite,
+                        ZipCode = addressVm.WebSite,
+                    };
 
-                dbContext.Companies.Add(company);
-                dbContext.Addresses.Add(companyAddress);
-                dbContext.SaveChanges();
+                    dbContext.Companies.Add(company);
+                    dbContext.Addresses.Add(companyAddress);
+                    dbContext.SaveChanges();
                 }
             }
             catch (Exception ex)
@@ -103,49 +104,49 @@ namespace HR.Web.Controllers
         {
             try
             {
-            using (HrDataContext dbContext = new HrDataContext())
-            {
-                Branch branch = new Branch()
+                using (HrDataContext dbContext = new HrDataContext())
                 {
-                    BranchCode = branchVm.branch.BranchCode,
-                    BranchName = branchVm.branch.BranchName,
-                    RegNo = branchVm.branch.RegNo,
-                    CreatedBy = "Admin",
-                    CreatedOn = DateTime.Now,
-                    ModifiedBy = "Admin",
-                    ModifiedOn = DateTime.Now,
-                    CompanyCode="EZY",
-                    CompanyId=1000
-                };
+                    Branch branch = new Branch()
+                    {
+                        BranchCode = branchVm.branch.BranchCode,
+                        BranchName = branchVm.branch.BranchName,
+                        RegNo = branchVm.branch.RegNo,
+                        CreatedBy = "Admin",
+                        CreatedOn = DateTime.Now,
+                        ModifiedBy = "Admin",
+                        ModifiedOn = DateTime.Now,
+                        CompanyCode = "EZY",
+                        CompanyId = 1000
+                    };
 
-                Address branchAddress = new Address()
-                {
-                    Address1 = addressVm.Address1,
-                    Address2 = addressVm.Address2,
+                    Address branchAddress = new Address()
+                    {
+                        Address1 = addressVm.Address1,
+                        Address2 = addressVm.Address2,
 
-                    AddressType = "Company",
-                    CityName = addressVm.CityName,
-                    Contact = addressVm.Contact,
-                    CountryCode = addressVm.CountryCode,
-                    LinkID = branch.BranchID,
-                    CreatedBy = "Admin",
-                    CreatedOn = DateTime.Now,
-                    Email = addressVm.Email,
-                    FaxNo = addressVm.FaxNo,
-                    MobileNo = addressVm.MobileNo,
-                    IsActive = true,
-                    ModifiedBy = "Admin",
-                    ModifiedOn = DateTime.Now,
-                    SeqNo = addressVm.SeqNo,
-                    StateName = addressVm.StateName,
-                    TelNo = addressVm.TelNo,
-                    WebSite = addressVm.WebSite,
-                    ZipCode = addressVm.WebSite,
-                };
-                dbContext.Branches.Add(branch);
-                dbContext.Addresses.Add(branchAddress);
-                dbContext.SaveChanges();
-            }
+                        AddressType = "Company",
+                        CityName = addressVm.CityName,
+                        Contact = addressVm.Contact,
+                        CountryCode = addressVm.CountryCode,
+                        LinkID = branch.BranchID,
+                        CreatedBy = "Admin",
+                        CreatedOn = DateTime.Now,
+                        Email = addressVm.Email,
+                        FaxNo = addressVm.FaxNo,
+                        MobileNo = addressVm.MobileNo,
+                        IsActive = true,
+                        ModifiedBy = "Admin",
+                        ModifiedOn = DateTime.Now,
+                        SeqNo = addressVm.SeqNo,
+                        StateName = addressVm.StateName,
+                        TelNo = addressVm.TelNo,
+                        WebSite = addressVm.WebSite,
+                        ZipCode = addressVm.WebSite,
+                    };
+                    dbContext.Branches.Add(branch);
+                    dbContext.Addresses.Add(branchAddress);
+                    dbContext.SaveChanges();
+                }
             }
             catch (Exception ex)
             {
@@ -153,6 +154,61 @@ namespace HR.Web.Controllers
                 throw ex;
             }
             return RedirectToAction("AddBranch");
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<AuthorViewModel> model = new List<AuthorViewModel>();
+
+            AuthorViewModel firstAuthor = new AuthorViewModel
+            {
+                Id = 1,
+                Name = "John",
+                BookViewModel = new List<BookViewModel>{
+                    new BookViewModel{
+                        Id=1,
+                        Title = "JQuery",
+                        IsWritten = false
+                    }, new BookViewModel{
+                        Id=1,
+                        Title = "JavaScript",
+                        IsWritten = false
+                    }
+                }
+            };
+
+            AuthorViewModel secondAuthor = new AuthorViewModel
+            {
+                Id = 2,
+                Name = "Deo",
+                BookViewModel = new List<BookViewModel>{
+                    new BookViewModel{
+                        Id=3,
+                        Title = "C#",
+                        IsWritten = false
+                    }, new BookViewModel{
+                        Id=4,
+                        Title = "Entity Framework",
+                        IsWritten = false
+                    }
+                }
+            };
+            model.Add(firstAuthor);
+            model.Add(secondAuthor);
+            return View("Index", model);
+        }
+        public class AuthorViewModel
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public List<BookViewModel> BookViewModel { get; set; }
+        }
+        public class BookViewModel
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public bool IsWritten { get; set; }
         }
     }
 }

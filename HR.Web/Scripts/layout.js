a289a117db8f9556292bb6f36bf315b85e631d2f
vijay <vijay@sulabs.tech>
2017-11-19 11:25:49 +0530

    btn css udpates

--- HR.Web/Scripts/layout.js
+++ HR.Web/Scripts/layout.js
@@ -116,3 +116,50 @@
 
     /* JS Utility Functions */
 });
+
+function createCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+function onNavBarClick() {
+    var cookieVal = readCookie('NavBarCookie');
+    if (cookieVal != null) {
+        var isOpen = cookieVal == 'open' ? true : false;
+        if (isOpen) {
+            createCookie('NavBarCookie', 'close', 300);
+        } else {
+            createCookie('NavBarCookie', 'open', 300);
+        }
+    } else {
+        createCookie('NavBarCookie', 'close', 300);
+    }    
+}
+/*
+createCookie('ppkcookie','testcookie',7);
+
+var x = readCookie('ppkcookie')
+if (x) {
+    [do something with x]
+}
+*/

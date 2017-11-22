9fa717c42ab5815f0f6b7fbadc559dcd7caa11d4
vijay <vijay@sulabs.tech>
2017-11-22 15:24:03 +0530

    sp added to the site

--- HR.Web/ViewModels/DashBoardVm.cs
+++ HR.Web/ViewModels/DashBoardVm.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using HR.Web.Models;
+
 namespace HR.Web.ViewModels
 {
     public class DashBoardVm
     {
         public int EmployeeCount { get; set; }
+        public IEnumerable<usp_EmployeeDateOfJoiningDate_Result> lineChartData { get; set; }
     }
 }

d839536209d6de2fd8b6514876f81a5d1562b095
vijay <vijay@sulabs.tech>
2017-12-01 19:17:57 +0530

    modal styles

--- HR.Web/Scripts/layout.js
+++ HR.Web/Scripts/layout.js
@@ -118,6 +118,19 @@
         increaseArea: '20%' // optional
     });
 
+    $('.modal .modal-dialog').attr('class', 'modal-dialog  bounceIn  animated');
+
+    //function testAnim(x) {
+    //    $('.modal .modal-dialog').attr('class', 'modal-dialog  ' + x + '  animated');
+    //};
+    //$('.modal-dialog').on('show.bs.modal', function (e) {
+        
+    //    testAnim('bounceIn');
+    //})
+    //$('.modal .modal-dialog').on('hide.bs.modal', function (e) {
+    //    //var anim = $('#exit').val();
+    //    testAnim('bounceOut');
+    //})
     /* JS Utility Functions */
 });
 

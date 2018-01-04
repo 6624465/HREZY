eb2c2830330018ae21cdc6f50dee4d9d7e7228ff
Maruthi Burkani <maruthiburkani@gmail.com>
2018-01-04 12:12:28 +0530

    new columns added : maruthi

--- HR.Web/Models/HrEntity.Designer.cs
+++ HR.Web/Models/HrEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Projects\HRNEW\HR.WEB\HR.Web\Models\HrEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\HRWEB\HR.Web\HR.Web\Models\HrEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.

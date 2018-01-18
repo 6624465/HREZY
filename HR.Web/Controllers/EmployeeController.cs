600c40997c0d0009fa8bda4fae884588cb971663
Naresh K <Naresh@sulabs.tech>
2018-01-18 18:07:30 +0530

    email changes

--- HR.Web/Controllers/EmployeeController.cs
+++ HR.Web/Controllers/EmployeeController.cs
@@ -298,7 +298,7 @@ namespace HR.Web.Controllers
         }
         public bool IsEmailExists(string Email)
         {
-            var list = empHeaderBO.GetListByProperty(x => x.UserEmailId == Email.ToLower()).ToList();
+            var list = empHeaderBO.GetListByProperty(x => x.UserEmailId.ToLower() == Email.ToLower()).ToList();
             int count = list.Count();
             return (count > 0 ? true : false);
         }

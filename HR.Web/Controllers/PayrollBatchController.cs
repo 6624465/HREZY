503d884b0a7f591e508a354b3c8bc30e08532489
Mahesh <guntimaheshyadav@gmail.com>
2018-02-15 19:04:54 +0530

    Payroll Batch : maruthi

--- HR.Web/Controllers/PayrollBatchController.cs
+++ HR.Web/Controllers/PayrollBatchController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HR.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,14 +7,20 @@ using System.Web.Mvc;
 
 namespace HR.Web.Controllers
 {
+    [SessionFilter]
     public class PayrollBatchController : Controller
     {
+        HrDataContext dbContext = new HrDataContext();
+
         // GET: PayrollBatch
         [HttpGet]
         public ActionResult ProcessPayroll()
         {
+            var list = dbContext.SalaryStructureHeaders.GroupJoin(dbContext.SalaryStructureDetails,
+                a=>a.StructureID, b=>b.StructureID,
+               (a,b) => new { A=a,B=b.ToList()});
             return View();
         }
-        
+
     }
 }

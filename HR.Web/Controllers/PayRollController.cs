8a42c66688fb20a8d61d7068628619c93c00179d
Kusumanjali <kusumanjalikorlapati@gmail.com>
2018-03-15 19:24:56 +0530

    changes

--- HR.Web/Controllers/PayRollController.cs
+++ HR.Web/Controllers/PayRollController.cs
@@ -1335,7 +1335,7 @@ namespace HR.Web.Controllers
             ViewData["documentsPath"] =
                    "TravlClaimDocs/" +
                    BRANCHID + "/" +
-                   EMPLOYEEID + "/" +
+                   travelobj.claimHeader.EmployeeId + "/" +
                   travelobj.claimHeader.ClaimNo;
             return PartialView("_PreviewTravelClaim", travelobj);
         }

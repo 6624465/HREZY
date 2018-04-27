94b93d9234a0ce4ada7c116262a1c9f0dac3c22a
Kusumanjali <kusumanjalikorlapati@gmail.com>
2018-04-27 12:17:25 +0530

    added Yearly Reports controller and views

---
+++ HR.Web/Controllers/YearlyReportsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HR.Web.Controllers
+{
+    public class YearlyReportsController : BaseController
+    {
+        // GET: YearlyReports
+       public ActionResult YearlyReportsTDS()
+        {
+            return View();
+        }
+        public ActionResult YearlyReportsSSC()
+        {
+            return View();
+        }
+        public ActionResult YearlyReportsOne()
+        {
+            return View();
+        }
+        public ActionResult YearlyReportsTwo()
+        {
+            return View();
+        }
+    }
+}

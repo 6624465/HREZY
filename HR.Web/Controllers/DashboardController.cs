59c6cfceec18e176310ab1fb05a04babc9b2f8a6
Prasad SKD <prasad.siripireddi@gmail.com>
2018-07-25 16:15:28 +0530

    commit changes

--- HR.Web/Controllers/DashboardController.cs
+++ HR.Web/Controllers/DashboardController.cs
@@ -531,10 +531,12 @@ namespace HR.Web.Controllers
             ViewData["FeMaleCount"] = femalCount;
             return View(employeeDataReportList);
         }
-        public ActionResult DashboardofSalaryComponents(int? BranchId,int? Year, byte? Month, int EmployeeId)
+        public ActionResult DashboardofSalaryComponents(int? BranchId, int? Year, byte? Month, int? EmployeeId)
         {
             BranchId = BranchId == 0 ? BRANCHID : BranchId;
-               SalaryComponantReportVm vm = new SalaryComponantReportVm();
+            Month = Month == 0 ? null : Month;
+            EmployeeId = EmployeeId == 0 ? null : EmployeeId;
+            SalaryComponantReportVm vm = new SalaryComponantReportVm();
             //vm.SalaryComponantReport = new List<USP_SALARYCOMPONENTREPORT_Result>();
             using (var dbCntx = new HrDataContext())
             {
